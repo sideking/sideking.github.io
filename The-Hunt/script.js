c5e446b3be90c365b0a36c9e36d464ecdc7a78f8
sideking <2234847@cmontmorency.qc.ca>
2023-09-22 13:26:25 -0400

    script change: object added

---
+++ The-Hunt/script.js
@@ -0,0 +1,71 @@
+let chapters = {
+    intro : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+
+    debut : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+
+    mort1 : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+
+    piste : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+
+    chercher : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+
+    combat : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+
+    mort2 : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+
+    decisif : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+
+    mort3 : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+
+    victoire : {
+        titre: "",
+        description: "",
+        image: "",
+        bouton: []
+    },
+}

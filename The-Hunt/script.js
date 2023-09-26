8dbd01bc085e2c372f6be5164f94a971b528baae
sideking <2234847@cmontmorency.qc.ca>
2023-09-26 14:13:25 -0400

    js story added

--- The-Hunt/script.js
+++ The-Hunt/script.js
@@ -1,41 +1,41 @@
 let chapters = {
     intro : {
         titre: "Mission Reçu",
-        description: "Vous avez reçu la mission de chasser un Diablos dans une région désertique",
+        description: "Vous avez reçu la mission de chasser un Diablos dans une région désertique. Étant membre d'une famille de chasseur vous partez vers le désert pour y rétablir l'équilibre.",
         image: "The-Hunt\Assets\Images\Diablos.jpg",
         bouton: []
     },
 
     debut : {
-        titre: "",
-        description: "",
+        titre: "La chasse commence",
+        description: "Vous arrivez sur le lieu de la mission, vous ne voyez pas le Diablos en question et le temps presse. Prenez-vous le temps d'envoyer un éclaireur, de partir en reconnaissance vous-même ou prend tu le risque de te dépêcher pour en finir rapidement",
         image: "",
         bouton: []
     },
 
-    ambuscade : {
-        titre: "",
-        description: "",
+    embuscade : {
+        titre: "Embuscade",
+        description: "Le diablos vous tend une embuscade pendant votre course erratique vers son repère.",
         image: "",
         bouton: []
     },
 
     piste : {
-        titre: "",
-        description: "",
+        titre: "Éclaireur",
+        description: "Vous Chercher pendant une demi-heure pour finalement trouver l'emplacement de la créature, il semble qu'elle était cachée dans le sable, attendant une proie.",
         image: "",
         bouton: []
     },
 
     chercher : {
-        titre: "",
-        description: "",
+        titre: "Chercher les alentours",
+        description: "Vous Chercher les alentours et trouvez des ruines datant de temps anciens. Vous y trouvez un objet ressemblant une bombe et la mettez dans une de vos poches",
         image: "",
         bouton: []
     },
 
     combat : {
-        titre: "",
+        titre: "Le Combat commence",
         description: "",
         image: "",
         bouton: []

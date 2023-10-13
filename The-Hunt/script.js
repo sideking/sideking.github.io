7eb43ba94cad2d4bb8e57037553c39649673185b
sideking <2234847@cmontmorency.qc.ca>
2023-10-13 11:34:08 -0400

    simplyfying

--- The-Hunt/script.js
+++ The-Hunt/script.js
@@ -1,10 +1,10 @@
-let chapters = {
+const chapters = {
     intro : {
         titre: "Mission Reçu",
         description: "Vous avez reçu la mission de chasser un Diablos dans une région désertique. Étant membre d'une famille de chasseur vous partez vers le désert pour y rétablir l'équilibre.",
         image: "./Assets/video/monster_showcase.mp4",
         bouton: [
-            {titre: 'S\'y rendre', "destination": "debut"}]
+            {titre: 'S\'y rendre', destination: "debut"}]
     },
 
     debut : {
@@ -12,9 +12,9 @@ let chapters = {
         description: "Vous arrivez sur le lieu de la mission, vous ne voyez pas le Diablos en question et le temps presse. Prenez-vous le temps d'envoyer un éclaireur, de partir en reconnaissance vous-même ou prend tu le risque de te dépêcher pour en finir rapidement.",
         image: "./Assets/video/welcome_my_friend.mp4",
         bouton: [
-            {titre: 'Vous dépêchez', "destination": "embuscade"}, 
-            {titre: 'Envoyer un éclaireur', "destination": "piste"},
-            {titre: 'Partir en reconnaissance', "destination": "piste"}]
+            {titre: 'Vous dépêchez', destination: "embuscade"}, 
+            {titre: 'Envoyer un éclaireur', destination: "piste"},
+            {titre: 'Partir en reconnaissance', destination: "piste"}]
     },
 
     embuscade : {
@@ -22,7 +22,7 @@ let chapters = {
         description: "Le diablos vous tend une embuscade pendant votre course erratique vers son repaire. Vous mourrez avant même de comprendre ce qu'il ce passe.",
         image: "./Assets/video/ambush.mp4",
         bouton: [
-            {titre: 'Vous êtes mort.', "destination": "debut"}]
+            {titre: 'Vous êtes mort.', destination: "debut"}]
     },
 
     piste : {
@@ -30,8 +30,8 @@ let chapters = {
         description: "Vous Chercher pendant une demi-heure pour finalement trouver l'emplacement de la créature, il semble qu'elle était cachée dans le sable, attendant une proie.",
         image: "./Assets/video/find_disablos.mp4",
         bouton: [
-            {titre: 'chercher les alentours', "destination": "chercher"}, 
-            {titre: 'Commencer le combat', "destination": "combat"}]
+            {titre: 'chercher les alentours', destination: "chercher"}, 
+            {titre: 'Commencer le combat', destination: "combat"}]
     },
 
     chercher : {
@@ -39,7 +39,7 @@ let chapters = {
         description: "Vous Chercher les alentours et trouvez des ruines datant de temps anciens. Vous y trouvez un objet ressemblant une bombe et la mettez dans une de vos poches.",
         image: "./Assets/video/find_bomb.mp4",
         bouton: [
-            {titre: 'Commencer le combat', "destination": "combat"}]
+            {titre: 'Commencer le combat', destination: "combat"}]
     },
 
     combat : {
@@ -47,8 +47,8 @@ let chapters = {
         description: "Ayant trouvé l'endroit où se cachait le Diablos, vous vous approchez discrètement et le prenez par surprise avec une attaque bien placée au niveau du dos. Le monstre se lève brusquement tout en vous précipitant plusieurs mètres dans les airs. Vous tombez sur vous pied et remaquez que le Diablos fonce vers vous.",
         image: "./Assets/video/surprise_attack.mp4",
         bouton: [
-            {titre: "Bloquer l'attaque", "destination": "bloquer"}, 
-            {titre: "Esquiver l'attaque", "destination": "esquiver"}]
+            {titre: "Bloquer l'attaque", destination: "bloquer"}, 
+            {titre: "Esquiver l'attaque", destination: "esquiver"}]
     },
 
     bloquer : {
@@ -56,7 +56,7 @@ let chapters = {
         description: "Voyant le Diablos se précipiter vers vous, votre corps réagi instinctivement et se met en position pour bloquer l'attaque du Diablos. Malheureusement pour vous, une des cornes du Diablos transperce facilement votre bouclier et se rend jusqu'à votre torse, vous mourrez en vous noyant dans votre propre sang.",
         image: "./Assets/video/block.mp4",
         bouton: [
-            {titre: 'Vous êtes mort.', "destination": "debut"}]
+            {titre: 'Vous êtes mort.', destination: "debut"}]
     },
 
     esquiver : {
@@ -64,8 +64,8 @@ let chapters = {
         description: "Voyant le Diablos se précipiter vers vous, vous vous jetez sur le côté tout en esquivant sa queue de justesse. Après vous être relevé vous apercevez que le Diablos est beaucoup plus puissant que ce qui était dit par le chef de la guilde et que vous ne pourrez pas gagner sans arme plus puissante.",
         image: "./Assets/video/dodge.mp4",
         bouton: [
-            {titre: "Continuer le combat", "destination": "vide"}, 
-            {titre: "Utiliser la bombe", "destination": "boom"}]
+            {titre: "Continuer le combat", destination: "vide"}, 
+            {titre: "Utiliser la bombe", destination: "boom"}]
     },
 
     vide : {
@@ -73,7 +73,7 @@ let chapters = {
         description: "Le Diablos fonce sur vous à nouveau sans vous laisser le temps de vous reposer. Vous essayer d'esquiver mais votre corps est trop fatigué pour vous écouter. Vous tombez sur le côté et le monstre vous frappe avec sa queue, écrasant votre coeur et les organes environnant.",
         image: "./Assets/video/no_bomb.mp4",
         bouton: [
-            {titre: 'Vous êtes mort.', "destination": "debut"}]
+            {titre: 'Vous êtes mort.', destination: "debut"}]
     },
 
     boom : {
@@ -81,7 +81,7 @@ let chapters = {
         description: "Vous sortez la bombe de votre sac et la lancez dans la bouche du monstre, le tuant directement en vous aspergeant de sang et de cervelle.",
         image: "./Assets/video/boom.mp4",
         bouton: [
-            {titre: 'charcuter le monstre', "destination": "victoire"}]
+            {titre: 'charcuter le monstre', destination: "victoire"}]
     },
 
     victoire : {
@@ -89,7 +89,7 @@ let chapters = {
         description: "Malgré l'état de votre personne, vous etes plutôt content d'Avoir survécu à cet affrontement.",
         image: "./Assets/video/charcuter.mp4",
         bouton: [
-            {titre: 'Retourner au village', "destination": "intro"}]
+            {titre: 'Retourner au village', destination: "intro"}]
     }
 };
 
@@ -114,28 +114,29 @@ function goToChapter(chapter) {
         while (button_Parent.firstChild) { 
             button_Parent.removeChild(button_Parent.firstChild); 
           } 
-        titre.innerText = chapters[chapter].titre;
-        videoSource.src = chapters[chapter].image;
-        paragraphe.innerText = chapters[chapter].description;
-        if (chapters[chapter] == chapters.intro) {
+          let chapterNow = chapters[chapter];
+        titre.innerText = chapterNow.titre;
+        videoSource.src = chapterNow.image;
+        paragraphe.innerText = chapterNow.description;
+        if (chapterNow == chapters.intro) {
             twist = false;
         }
-        if (chapters[chapter] == chapters.chercher) {
+        if (chapterNow == chapters.chercher) {
             twist = true;
         }
-        if (chapters[chapter] == chapters.esquiver && twist == false) {
+        if (chapterNow == chapters.esquiver && twist == false) {
             const nouveauBtn = document.createElement('button'); 
-            nouveauBtn.textContent = chapters[chapter].bouton[0].titre; 
+            nouveauBtn.textContent = chapterNow.bouton[0].titre; 
             nouveauBtn.addEventListener('click', () => { 
-              goToChapter(chapters[chapter].bouton[0].destination) 
+              goToChapter(chapterNow.bouton[0].destination) 
             }); 
             button_Parent.appendChild(nouveauBtn); 
         } else {
-        for (const i in chapters[chapter].bouton) {
+        for (const i in chapterNow.bouton) {
             const nouveauBtn = document.createElement('button'); 
-            nouveauBtn.textContent = chapters[chapter].bouton[i].titre; 
+            nouveauBtn.textContent = chapterNow.bouton[i].titre; 
             nouveauBtn.addEventListener('click', () => { 
-              goToChapter(chapters[chapter].bouton[i].destination) 
+              goToChapter(chapterNow.bouton[i].destination) 
             }); 
             button_Parent.appendChild(nouveauBtn); 
         }}

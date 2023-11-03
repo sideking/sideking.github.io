8bdc822d086f6c56907bdd9836f4d24a234f65b1
sideking <2234847@cmontmorency.qc.ca>
2023-11-03 13:21:51 -0400

    song 2 fix

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,7 +2,8 @@ const chapters = {
     intro: {
         titre: "Mission Reçu",
         description: "Vous avez reçu la mission de chasser un Diablos dans une région désertique. Étant membre d’une famille de chasseurs, vous partez vers le désert pour y rétablir l’équilibre.",
-        image: "./Assets/video/monster_showcase.mp4",
+        video: "./Assets/video/monster_showcase.mp4",
+        bruit: "",
         bouton: [
             { titre: 'S\'y rendre', destination: "debut" }]
     },
@@ -10,7 +11,8 @@ const chapters = {
     debut: {
         titre: "La chasse commence",
         description: "Vous arrivez sur le lieu de la mission, vous ne voyez pas le Diablos en question et le temps presse. Prenez-vous le temps d’envoyer un éclaireur, de partir en reconnaissance vous-même ou prend-tu le risque de te dépêcher pour en finir rapidement.",
-        image: "./Assets/video/welcome_my_friend.mp4",
+        video: "./Assets/video/welcome_my_friend.mp4",
+        bruit: "",
         bouton: [
             { titre: 'Vous dépêchez', destination: "embuscade" },
             { titre: 'Envoyer un éclaireur', destination: "piste" },
@@ -20,7 +22,8 @@ const chapters = {
     embuscade: {
         titre: "Embuscade",
         description: "Le diablos vous tend une embuscade pendant votre course erratique vers son repaire. Vous mourrez avant même de comprendre ce qu’il se passe.",
-        image: "./Assets/video/ambush.mp4",
+        video: "./Assets/video/ambush.mp4",
+        bruit: "",
         bouton: [
             { titre: 'Vous êtes mort.', destination: "debut" }]
     },
@@ -28,7 +31,8 @@ const chapters = {
     piste: {
         titre: "Reconnaissance",
         description: "Vous cherchez pendant une demi-heure pour finalement trouver l’emplacement de la créature, il semble qu’elle était cachée dans le sable, attendant une proie.",
-        image: "./Assets/video/find_disablos.mp4",
+        video: "./Assets/video/find_disablos.mp4",
+        bruit: "",
         bouton: [
             { titre: 'chercher les alentours', destination: "chercher" },
             { titre: 'Commencer le combat', destination: "combat" }]
@@ -37,7 +41,8 @@ const chapters = {
     chercher: {
         titre: "Chercher les alentours",
         description: "Vous cherchez les alentours et trouvez des ruines datant de temps anciens. Vous y trouvez un objet ressemblant à une bombe et la mettez dans une de vos poches.",
-        image: "./Assets/video/find_bomb.mp4",
+        video: "./Assets/video/find_bomb.mp4",
+        bruit: "",
         bouton: [
             { titre: 'Commencer le combat', destination: "combat" }]
     },
@@ -45,7 +50,8 @@ const chapters = {
     combat: {
         titre: "Le Combat commence",
         description: "Ayant trouvé l’endroit où se cachait le Diablos, vous vous approchez discrètement et le prenez par surprise avec une attaque bien placée au niveau du dos. Le monstre se lève brusquement tout en vous précipitant plusieurs mètres dans les airs. Vous tombez sur vous pied et remarquez que le Diablos fonce vers vous.",
-        image: "./Assets/video/surprise_attack.mp4",
+        video: "./Assets/video/surprise_attack.mp4",
+        bruit: "",
         bouton: [
             { titre: "Bloquer l'attaque", destination: "bloquer" },
             { titre: "Esquiver l'attaque", destination: "esquiver" }]
@@ -54,7 +60,8 @@ const chapters = {
     bloquer: {
         titre: "Fer contre corne",
         description: "Voyant le Diablos se précipiter vers vous, votre corps réagit instinctivement et se met en position pour bloquer l’attaque du Diablos. Malheureusement pour vous, une des cornes du Diablos transperce facilement votre bouclier et se rend jusqu’à votre torse, vous mourrez en vous noyant dans votre propre sang.",
-        image: "./Assets/video/block.mp4",
+        video: "./Assets/video/block.mp4",
+        bruit: "",
         bouton: [
             { titre: 'Vous êtes mort.', destination: "debut" }]
     },
@@ -62,7 +69,8 @@ const chapters = {
     esquiver: {
         titre: "Esquiver de justesse",
         description: "Voyant le Diablos se précipiter vers vous, vous vous jetez sur le côté tout en esquivant sa queue de justesse. Après vous être relevé, vous apercevez que le Diablos est beaucoup plus puissant que ce qui était dit par le chef de la guilde et que vous ne pourrez pas gagner sans arme plus puissante.",
-        image: "./Assets/video/dodge.mp4",
+        video: "./Assets/video/dodge.mp4",
+        bruit: "",
         bouton: [
             { titre: "Continuer le combat", destination: "vide" },
             { titre: "Utiliser la bombe", destination: "boom" }]
@@ -71,7 +79,8 @@ const chapters = {
     vide: {
         titre: "Il va falloir faire sans",
         description: "Le Diablos fonce sur vous à nouveau sans vous laisser le temps de vous reposer. Vous essayez d’esquiver, mais votre corps est trop fatigué pour vous écouter. Vous tombez sur le côté et le monstre vous frappe avec sa queue, écrasant votre cœur et les organes environnants.",
-        image: "./Assets/video/no_bomb.mp4",
+        video: "./Assets/video/no_bomb.mp4",
+        bruit: "",
         bouton: [
             { titre: 'Vous êtes mort.', destination: "debut" }]
     },
@@ -79,7 +88,8 @@ const chapters = {
     boom: {
         titre: "Une explosion à tout casser",
         description: "Vous sortez la bombe de votre sac et la lancez dans la bouche du monstre, le tuant directement en vous aspergeant de sang et de cervelle.",
-        image: "./Assets/video/boom.mp4",
+        video: "./Assets/video/boom.mp4",
+        bruit: "",
         bouton: [
             { titre: 'charcuter le monstre', destination: "victoire" }]
     },
@@ -87,15 +97,18 @@ const chapters = {
     victoire: {
         titre: "Une mission rondement menée",
         description: "Malgré l’état de votre personne, vous êtes plutôt content d’avoir survécu à cet affrontement.",
-        image: "./Assets/video/charcuter.mp4",
+        video: "./Assets/video/charcuter.mp4",
+        bruit: "",
         bouton: [
             { titre: 'Retourner au village', destination: "intro" }]
     }
 };
 
 //audio pour ne pas devenir sourd
-let song = document.querySelector("audio");
+let song = document.querySelector("#musique");
 song.volume = 0.1;
+let questStart = "./Assets/music/Proof of a Hero.mp3";
+let questEnd = "./Assets/music/Monster Hunter_ World - Quest Clear [Music].mp3";
 
 //twist
 let twist = false;
@@ -114,12 +127,20 @@ function goToChapter(chapter) {
         }
         let chapterNow = chapters[chapter];
         titre.innerText = chapterNow.titre;
-        videoSource.src = chapterNow.image;
+        videoSource.src = chapterNow.video;
         paragraphe.innerText = chapterNow.description;
         if (chapterNow == chapters.debut) {
-            song.play();
+            song.src = questStart
+        } else if (chapterNow == chapters.victoire) {
+            song.src = questEnd
         }
         if (chapterNow == chapters.intro) {
+
+        } else {
+            song.play()
+        }
+        
+        if (chapterNow == chapters.debut) {
             twist = false;
         }
         if (chapterNow == chapters.chercher) {
